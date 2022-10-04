e048dec733be46472ff19b173ade25ba56fa9f13
Peter Harrison <peterharrison.pch@gmail.com>
2022-10-03 23:27:28 -0700

    commit for question two

--- q2.py
+++ q2.py
@@ -1,8 +1,18 @@
+unleash = True
+longest = ""
+shortest = "supercalifragilistiestpealidoish" * 4
 
-# ******************************
-# Make your Code
-# ******************************
+while unleash:
+    text = input()
+    if text == "stop" or text == "STOP":
+        unleash = False
+        break
+    if len(text) > len(longest):
+        longest = text
+    if len(text) < len(shortest):
+        shortest = text
 
+print(longest, shortest)
 
 # Requirement
 # No need to use list
